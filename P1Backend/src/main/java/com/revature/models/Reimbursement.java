6e884a868705874d9f316bde9b85d33f4baced4e
William Nettles <willnettles@gmail.com>
2024-04-26 19:05:18 -0400

    model classes

--- P1Backend/src/main/java/com/revature/models/Reimbursement.java
+++ P1Backend/src/main/java/com/revature/models/Reimbursement.java
@@ -1,4 +1,92 @@
 package com.revature.models;
 
+import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Id;
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+@Table(name = "reimbursements")
 public class Reimbursement {
+
+    @GeneratedValue
+    @Id
+    private int reimbId;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private double amount;
+
+    @Column(nullable = false)
+    private int status;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @Autowired
+    public Reimbursement() {
+    }
+
+    public Reimbursement(int reimbId, String description, double amount, int status, User user) {
+        this.reimbId = reimbId;
+        this.description = description;
+        this.amount = amount;
+        this.status = status;
+        this.user = user;
+    }
+
+    public int getReimbId() {
+        return reimbId;
+    }
+
+    public void setReimbId(int reimbId) {
+        this.reimbId = reimbId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Reimbursement{" +
+                "reimbId=" + reimbId +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                ", status=" + status +
+                ", user=" + user +
+                '}';
+    }
 }
