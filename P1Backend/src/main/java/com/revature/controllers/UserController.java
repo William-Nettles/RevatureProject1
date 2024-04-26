e3c2b8ecdeb9d9112c7d2136b3f319c362f1ebe9
kemosabe1900 <mati319@revature.net>
2024-04-26 18:54:59 -0500

    test push

--- P1Backend/src/main/java/com/revature/controllers/UserController.java
+++ P1Backend/src/main/java/com/revature/controllers/UserController.java
@@ -2,10 +2,19 @@ package com.revature.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.revature.daos.UserDAO;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    UserDAO userDAO;
+
+    @Autowrired
+    public userController(UserDAO userDAO){
+        this.userDAO =userDAO
+    }
+
 
 
 }
