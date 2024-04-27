14c8dcbf2f2109a4844b8167278d9ff9ddae59a0
kemosabe1900 <mati319@revature.net>
2024-04-26 20:41:53 -0500

    User side methods

--- P1Backend/src/main/java/com/revature/controllers/UserController.java
+++ P1Backend/src/main/java/com/revature/controllers/UserController.java
@@ -1,8 +1,15 @@
 package com.revature.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.revature.daos.ReimbursementDAO;
+import com.revature.models.Reimbursement;
+import com.revature.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.revature.daos.UserDAO;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -10,11 +17,32 @@ public class UserController {
 
     UserDAO userDAO;
 
-    @Autowrired
-    public userController(UserDAO userDAO){
-        this.userDAO =userDAO
+    @Autowired
+    public UserController(UserDAO userDAO){
+        this.userDAO =userDAO;
     }
 
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.status(201).body(userDAO.save(user));
+    }
 
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User>users = userDAO.findAll();
+        return ResponseEntity.ok(users);
+    }
 
+    @DeleteMapping("/userId")
+    public ResponseEntity<Object> deleteUser(@PathVariable int userId){
+        Optional<User> u = userDAO.findById(userId);
+
+        if(u.isEmpty()){
+            return ResponseEntity.status(404).body("UserId not found");
+        }
+        User user = u.get();
+        userDAO.deleteById(userId);
+
+        return ResponseEntity.accepted().body("user deleted");
+    }
 }
--- P1Backend/src/main/java/com/revature/service/UserService.java
+++ P1Backend/src/main/java/com/revature/service/UserService.java
@@ -1,7 +1,13 @@
 package com.revature.service;
 
 import com.revature.daos.UserDAO;
+import com.revature.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
 
 public class UserService {
 
@@ -10,4 +16,29 @@ public class UserService {
     @Autowired
     public UserService() {
     }
+
+
+    public ResponseEntity<User> createUser(User user) {
+        return ResponseEntity.status(201).body(userDAO.save(user));
+    }
+
+
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User>users = userDAO.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+
+    public ResponseEntity<Object> deleteUser(int userId){
+        Optional<User> u = userDAO.findById(userId);
+
+        if(u.isEmpty()){
+            return ResponseEntity.status(404).body("UserId not found");
+        }
+        User user = u.get();
+        userDAO.deleteById(userId);
+
+        return ResponseEntity.accepted().body("user deleted");
+    }
+
 }
