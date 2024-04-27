5004eab59c149de1e9a764aa6572762dc38c2586
William Nettles <willnettles@gmail.com>
2024-04-26 20:42:04 -0400

    worked some on ReimbursementController

--- P1Backend/src/main/java/com/revature/controllers/ReimbursementController.java
+++ P1Backend/src/main/java/com/revature/controllers/ReimbursementController.java
@@ -1,9 +1,12 @@
 package com.revature.controllers;
 
+import com.revature.models.Reimbursement;
 import com.revature.service.ReimbursementService;
 import com.revature.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reimbursements")
@@ -15,6 +18,26 @@ public class ReimbursementController {
     public ReimbursementController() {
         reimServ = new ReimbursementService();
         userServ = new UserService();
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createNewReimbursement(@RequestBody Reimbursement reimbursement) {
+        return reimServ.createReimbursement(reimbursement);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Reimbursement>> getAllReimbursements() {
+        return reimServ.getAllReims();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Reimbursement>> getPendingReimbursements() {
+        return reimServ.getAllPending();
+    }
+
+    @PatchMapping("/{reimId}")
+    public ResponseEntity<Object> updateReimbursementStatus(@PathVariable int reimId, @RequestBody int status) {
+        return reimServ.updateStatus(reimId, status);
     }
 
 
--- P1Backend/src/main/java/com/revature/service/ReimbursementService.java
+++ P1Backend/src/main/java/com/revature/service/ReimbursementService.java
@@ -1,7 +1,12 @@
 package com.revature.service;
 
 import com.revature.daos.ReimbursementDAO;
+import com.revature.models.Reimbursement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
 
 public class ReimbursementService {
 
@@ -10,5 +15,29 @@ public class ReimbursementService {
     @Autowired
     public ReimbursementService() {
 
+    }
+
+    public ResponseEntity<Object> createReimbursement(Reimbursement reimbursement) {
+        //TODO: add error handling logic
+        return ResponseEntity.status(201).body(reimDAO.save(reimbursement));
+    }
+
+    public ResponseEntity<List<Reimbursement>> getAllReims() {
+        //TODO: if user return only user's, if manager, return all
+        return ResponseEntity.ok(reimDAO.findAll());
+    }
+
+    public ResponseEntity<List<Reimbursement>> getAllPending() {
+        return ResponseEntity.ok(reimDAO.findAll().stream().filter((reim)->{return reim.getStatus()==0;}).toList());
+    }
+
+
+    public ResponseEntity<Object> updateStatus(int reimId, int status) {
+        Optional<Reimbursement> rb = reimDAO.findById(reimId);
+        if (rb.isEmpty())
+            return ResponseEntity.badRequest().body("No Reimbursement with Id: " + reimId +" found");
+        Reimbursement reimbursement = rb.get();
+        reimbursement.setStatus(status);
+        return ResponseEntity.ok(reimDAO.save(reimbursement));
     }
 }
